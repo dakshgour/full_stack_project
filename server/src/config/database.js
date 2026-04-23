0e124c159e201932532405903f6eccb2fe4ba356
Daksh Gour <dakshgour44@gmail.com>
2026-04-23 17:58:26 +0530

    Fix local dev runtime issues

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,13 +1,16 @@
-import mysql from 'mysql2/promise';
 import { env } from './env.js';
 
 export async function createDatabasePool() {
-  if (env.mysqlUrl) {
-    return mysql.createPool(env.mysqlUrl);
+  const hasUrl = Boolean(env.mysqlUrl);
+  const hasDiscreteConfig = Boolean(env.mysqlHost && env.mysqlUser && env.mysqlDatabase);
+  if (!hasUrl && !hasDiscreteConfig) {
+    return null;
   }
 
-  if (!env.mysqlHost || !env.mysqlUser || !env.mysqlDatabase) {
-    return null;
+  const mysql = await import('mysql2/promise');
+
+  if (hasUrl) {
+    return mysql.createPool(env.mysqlUrl);
   }
 
   return mysql.createPool({
